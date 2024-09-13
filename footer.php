8457b5e43a3967242fff61e6d67b7bee8882d16d
La <32545740+lanuxos@users.noreply.github.com>
2024-09-13 23:01:44 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container mt-1">
         <span class="darkolivegreen text-center">Copyright ©
-            <a href="index.php" class="darkolivegreen"> ກົມອຸປະໂພກ - ບໍລິໂພກ ກົມໃຫຍ່ພະລາທິການກອງທັບ</a>., All rights
+            <a href="index.php" class="darkolivegreen"> PHP Document Management System</a>., All rights
             reserved | <a href="mailto:lanuxos@gmail.com" class="darkolivegreen">Admin: La</a></span>
     </div>
 </footer>
